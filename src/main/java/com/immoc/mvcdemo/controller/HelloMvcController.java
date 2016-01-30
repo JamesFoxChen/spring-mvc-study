0802d0f545f3053cc2e9fc08f2b0d41e05a0af66
2517594@qq.com <ch1987hzgit>
2016-01-31 00:35:00 +0800

    spring mvc study initial

---
+++ src/main/java/com/immoc/mvcdemo/controller/HelloMvcController.java
@@ -0,0 +1,16 @@
+package com.immoc.mvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloMvcController {
+
+	//http://local:8080/hello/mvc
+	@RequestMapping("/mvc")
+	public String helloMvc()
+	{
+		return "home";
+	}
+}
